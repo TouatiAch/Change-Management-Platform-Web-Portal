3676c17da86a8e7a47421cb7f582ee35ad729fd2
TouatiAch <twetiachref2@gmail.com>
2025-04-10 17:22:00 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useMsal } from "@azure/msal-react";
 
 function App() {
+  const { instance } = useMsal();
+
+  const handleLogin = () => {
+    instance.loginRedirect();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 50 }}>
+      <h1>Change Management Portal</h1>
+      <button onClick={handleLogin}>Login with Microsoft</button>
     </div>
   );
 }
---
+++ src/authConfig.js
@@ -0,0 +1,8 @@
+export const msalConfig = {
+    auth: {
+      clientId: "17d45425-fdd2-42ea-aa96-37a92dc49794", // from Azure portal
+      authority: "https://login.microsoftonline.com/common",
+      redirectUri: "https://github.com/TouatiAch/Change-Management-Platform-Web-Portal",
+    },
+  };
+  
